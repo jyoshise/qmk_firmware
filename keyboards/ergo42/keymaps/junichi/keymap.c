7b7d914d946b38773a3adf97f05802b0d7e46046
jyoshise <yoshise@gmail.com>
2018-09-20 17:34:05 +0900

    remap thumb keys for ergo42

--- keyboards/ergo42/keymaps/junichi/keymap.c
+++ keyboards/ergo42/keymaps/junichi/keymap.c
@@ -20,14 +20,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Esc  |   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Enter |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust| Caps |  GUI | Alt  |Shift |Lower |Space |Space |Raise |Shift | Left | Down |  Up  |Right |
+   * |Adjust| Caps |  GUI | Alt  | Alt  |Lower |Shift/Space |Shift/Space |Raise |Alt | Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [QWERTY] = LAYOUT( \
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_RBRC,       KC_BSLS,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
-    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    S(KC_8),       S(KC_9),    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
-    KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    S(KC_RBRC),    S(KC_BSLS), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-    _______, KC_CAPS, KC_LGUI, KC_LALT, KC_LSFT, MO(LOWER),   KC_SPC,        KC_SPC,     MO(RAISE),   KC_RSFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,       KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
+    KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    S(KC_9),       S(KC_0),    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
+    KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    S(KC_LBRC),    S(KC_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
+    _______, KC_CAPS, _______, KC_LGUI, KC_LALT, MO(LOWER),   LSFT_T(KC_SPC),        RSFT_T(KC_SPC),     MO(RAISE),   KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 
