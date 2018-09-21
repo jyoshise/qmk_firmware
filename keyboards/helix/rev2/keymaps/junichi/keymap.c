11851b6d07fda97b77bd4c2cdc83bfff159b9a4d
Junichi Yoshise <yoshise@gmail.com>
2018-09-21 17:07:50 +0900

    modify helix thumb key

--- keyboards/helix/rev2/keymaps/junichi/keymap.c
+++ keyboards/helix/rev2/keymaps/junichi/keymap.c
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
       KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_ESC,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT , \
-      ADJUST,  KC_CAPS, KC_LGUI, KC_LALT, KC_LSFT, LOWER,   KC_SPC,  KC_SPC,  RAISE,   KC_RSFT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      ADJUST,  KC_CAPS, _______, KC_LGUI, KC_LALT, LOWER, LSFT_T(KC_SPC), RSFT_T(KC_SPC), RAISE,  KC_RALT, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
       ),
 
   /* Lower
