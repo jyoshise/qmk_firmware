4d8a58b1bd96e0c868713b6fbbedc41f280d6240
jyoshise <yoshise@gmail.com>
2018-10-20 15:51:19 +0900

    add adjust layer for ergo42

--- keyboards/ergo42/keymaps/junichi/keymap.c
+++ keyboards/ergo42/keymaps/junichi/keymap.c
@@ -5,6 +5,7 @@ extern keymap_config_t keymap_config;
 #define QWERTY 0
 #define LOWER 1
 #define RAISE 2
+#define ADJUST 3
 
 // Fillers to make layering more clear
 #define _______ KC_TRNS
@@ -27,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_LBRC,       KC_RBRC,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    S(KC_9),       S(KC_0),    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
     LSFT_T(KC_ESC),  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    S(KC_LBRC),    S(KC_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT), \
-    _______, KC_CAPS, _______, KC_LGUI, KC_LALT, MO(LOWER),   KC_SPC,        KC_SPC,     MO(RAISE),   LCTL(KC_SPC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+    MO(ADJUST), KC_CAPS, _______, KC_LGUI, KC_LALT, MO(LOWER),   KC_SPC,        KC_SPC,     MO(RAISE),   LCTL(KC_SPC), KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
 ),
 
 
@@ -46,7 +47,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, _______, _______, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, KC_COMM, KC_DOT, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
+      _______, _______, _______, _______, _______, _______, _______, _______, MO(ADJUST), _______, KC_MNXT, KC_VOLD, KC_VOLU, KC_MPLY \
       ),
 
   /* Raise
@@ -64,7 +65,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    _______, _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
       _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   _______, _______, KC_F6,   KC_MINS, KC_EQL,  KC_LBRC, KC_RBRC, KC_BSLS, \
       _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  _______, _______, KC_F12,  _______, KC_COMM, KC_DOT, _______, _______, \
-      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______\
-      )
+      _______, _______, _______, _______, _______, MO(ADJUST), _______, _______, _______, _______, _______, KC_PGDN, KC_PGUP, _______\
+    ),
+
+  /* Adjust (Lower + Raise)
+   * ,-----------------------------------------.             ,-----------------------------------------.
+   * |      | Reset|      |      |      |      |             |      |      |      |      |      |  Del |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |      |      |Aud on|Audoff| Mac  |             | LEFT | DOWN | UP   | RIGHT|      |      |
+   * |------+------+------+------+------+------|             |------+------+------+------+------+------|
+   * |      |      |      |      |      |      |             |      |RGBRST|RGB ON| HUE+ | SAT+ | VAL+ |
+   * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
+   * |      |      |      |      |      |      |      |      |      |      | MODE | HUE- | SAT- | VAL- |
+   * `-------------------------------------------------------------------------------------------------'
+   */
+  [ADJUST] =  LAYOUT( \
+      _______, RESET,   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL, \
+      _______, _______, _______, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,  _______, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+    )
 
 };
