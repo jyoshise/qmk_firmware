0f1515be0664304c7d8448f504baab11364bc67e
jyoshise <yoshise@gmail.com>
2018-11-29 04:11:03 +0900

    fix

--- keyboards/helix/rev2/keymaps/junichi/keymap.c
+++ keyboards/helix/rev2/keymaps/junichi/keymap.c
@@ -62,14 +62,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------|             |------+------+------+------+------+------|
    * | Shift|   Z  |   X  |   C  |   V  |   B  |             |   N  |   M  |   ,  |   .  |   /  |Shift |
    * |------+------+------+------+------+------+-------------+------+------+------+------+------+------|
-   * |Adjust| Caps |      | GUI  | Alt  |Lower |Space |Enter |Raise | Alt  | Left | Down |  Up  |Right |
+   * |Adjust| Caps |      |      |GUI/MH|Lower |Space |Enter |Raise |Alt/HK| Left | Down |  Up  |Right |
    * `-------------------------------------------------------------------------------------------------'
    */
   [_QWERTY] = LAYOUT( \
       KC_ESC,         KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                  KC_Y,    KC_U,        KC_I,    KC_O,    KC_P,    KC_BSPC, \
       LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                  KC_H,    KC_J,        KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
       KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                  KC_N,    KC_M,        KC_COMM, KC_DOT,  KC_SLSH, RSFT_T(KC_ENT), \
-      ADJUST,         KC_CAPS, _______, KC_LGUI, KC_LALT, LOWER, KC_SPC, KC_ENT, RAISE,   LCTL(KC_SPC),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
+      ADJUST,         KC_CAPS, _______, _______, GUI_T(KC_INT5), LOWER, KC_SPC, KC_ENT, RAISE,ALT_T(KC_INT4),KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT \
     ),
 
   /* Lower
